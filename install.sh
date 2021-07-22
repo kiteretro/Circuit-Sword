4ca8d9feae7b787ab399cc019bfc4230acc740db
Antho91 <basverheule1991@gmail.com>
2021-07-22 19:55:32 +0200

    Update install.sh
    For testing purpose, change the GitHub URL

--- install.sh
+++ install.sh
@@ -46,7 +46,7 @@ else
 fi
 
 GITHUBPROJECT="Circuit-Sword"
-GITHUBURL="https://github.com/kiteretro/$GITHUBPROJECT"
+GITHUBURL="https://github.com/Antho91/$GITHUBPROJECT"
 PIHOMEDIR="$DEST/home/pi"
 BINDIR="$PIHOMEDIR/$GITHUBPROJECT"
 USER="pi"
